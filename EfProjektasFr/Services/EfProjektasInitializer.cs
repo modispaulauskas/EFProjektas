b5d4c81784834542e403f5fb03870ee7307a753b
Modestas Paulauskas <modispaulauskas@gmail.com>
2021-01-28 20:35:52 +0200

    init

--- EfProjektasFr/Services/EfProjektasInitializer.cs
+++ EfProjektasFr/Services/EfProjektasInitializer.cs
@@ -1,4 +1,5 @@
-﻿using EFAiskinimas.Common.InitialData;
+﻿using EFAiskinimas.Common;
+using EFAiskinimas.Common.InitialData;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -15,10 +16,22 @@ namespace EfProjektasFr.Services
             context.Students.AddRange(StudentInitialData.DataSeed);
             context.Professions.AddRange(ProfessionInitialData.DataSeed);
             context.Persons.AddRange(PersonInitialData.DataSeed);
-            context.Hobbies.AddRange(HobbyInitialData.DataSeed);
             context.Dormitories.AddRange(DormitoryInitialData.DataSeed);
             context.Courses.AddRange(CourseInitialData.DataSeed);
 
+            foreach (var item in HobbyInitialData.DataSeed)
+            {
+                
+                string[] temp = item.Split(',');
+                Hobby HData = new Hobby
+                {
+                    HobbyId = int.Parse(item[0].ToString()),
+                    Text = item[1].ToString(),
+                    TextLt = item[2].ToString()
+                };
+                context.Hobbies.Add(HData);
+            }
+            
         }
     }
 }
