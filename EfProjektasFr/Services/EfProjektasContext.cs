009e92e8fca438bae43f76a3e0427160f3970047
Modestas Paulauskas <modispaulauskas@gmail.com>
2021-01-28 19:17:00 +0200

    Add project files.

---
+++ EfProjektasFr/Services/EfProjektasContext.cs
@@ -0,0 +1,32 @@
+﻿using EFAiskinimas.Common;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfProjektasFr.Services
+{
+    public class EfProjektasContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Hobby> Hobbies { get; set; }
+        public DbSet<Profession> Professions { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Dormitory> Dormitories { get; set; }
+        public DbSet<Enrolment> Enrolments { get; set; }
+        public DbSet<Location> Locations { get; set; }
+
+        public EfProjektasContext() : base("StudentDb")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<>
+        }
+    }
+}
